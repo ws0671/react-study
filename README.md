a047edf5a5ea6d3395e0e7ce8f2272a48679bceb
Yeongmin Lee <ws0671@naver.com>
2025-04-04 17:33:22 +0900

    desktop 화면 수정 및 readme 추가가

---
+++ README.md
@@ -0,0 +1,2 @@
+1. https://rsp-one.vercel.app
+2. https://react-study-pied-beta.vercel.app/
--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -83,15 +83,16 @@ function App() {
   return (
     <>
       {loading ? (
-        <div className="text-2xl">로딩중...</div>
+        <div className="h-screen flex  p-8 bg-linear-to-b from-cyan-500 to-blue-500 justify-center items-center font-custom font-bold">
+          Now Loading...
+        </div>
       ) : data ? (
         <div className="h-screen flex  p-8 bg-linear-to-b from-cyan-500 to-blue-500 justify-center items-center font-custom font-bold ">
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-5 sm:grid sm:grid-cols-3 sm:grid-rows-2 w-[50rem]">
             <div
               aria-label="temp_card"
               className="w-full rounded-xl bg-amber-400  text-center p-5 "
             >
-              <div></div>
               <div className="flex gap-3 justify-center mb-3 ">
                 <div></div>
                 <div className="text-7xl">{`${data?.main?.temp.toFixed()}`}</div>
@@ -113,7 +114,7 @@ function App() {
                   </div>
                 </div>
               </div>
-              <div className=" text-lg items-center justify-center grid grid-cols-3">
+              <div className=" text-lg sm:text-sm items-center justify-center grid grid-cols-3">
                 <img
                   className="w-8 justify-self-end"
                   src={`https://openweathermap.org/img/wn/${data?.weather[0]?.icon}.png`}
@@ -133,18 +134,25 @@ function App() {
               </div>
             </div>
             <div
-              aria-label="weather_card"
-              className="w-full  p-3 flex flex-col justify-center items-center text-3xl gap-3  bg-amber-700 rounded-xl"
+              aria-label="state_card"
+              className="w-full  p-3 flex flex-col justify-center items-center text-3xl text-center gap-3  bg-amber-700 rounded-xl"
             >
               {/* <div>{data?.weather[0]?.main}</div> */}
               <div>{data?.weather[0]?.description}</div>
             </div>
-            <div className="w-full bg-white col-span-2 rounded-xl  flex gap-6 p-3 justify-center items-center">
+            <div
+              aria-label="wind_humidity_card"
+              className="w-full bg-white rounded-xl  flex gap-6 p-3 justify-center items-center"
+            >
               <div className="flex items-center  justify-center gap-3">
                 <div className="w-5 ">
                   <img src="/images/wind.png" alt="" />
                 </div>
-                <div>{data.wind.speed}m/s</div>
+                {isFahrenheit ? (
+                  <div>{data.wind.speed}mph</div>
+                ) : (
+                  <div>{data.wind.speed}m/s</div>
+                )}
               </div>
               <div className="flex items-center  justify-center gap-3">
                 <div className="w-5">
@@ -153,48 +161,53 @@ function App() {
                 <div>{data.main.humidity}%</div>
               </div>
             </div>
-            <div className="w-full rounded-xl bg-black ">
+            <div className=" sm:col-span-3">
               <div
-                className=" text-white w-full border-b-1  p-3 flex justify-center items-center cursor-pointer"
-                onClick={() => {
-                  setText("");
-                }}
+                aria-label="location_card"
+                className="w-full rounded-xl bg-black"
               >
-                Current location
-              </div>
-              <div className="w-full text-white grid grid-cols-4 ">
-                <button
-                  className="border-r-1  p-3 cursor-pointer"
+                <div
+                  className=" text-white w-full border-b-1  p-3 flex justify-center items-center cursor-pointer"
                   onClick={() => {
-                    setText("seoul");
+                    setText("");
                   }}
                 >
-                  Seoul
-                </button>
-                <button
-                  className="p-3 cursor-pointer"
-                  onClick={() => {
-                    setText("tokyo");
-                  }}
-                >
-                  Tokyo
-                </button>
-                <button
-                  className="border-x-1  p-3 cursor-pointer"
-                  onClick={() => {
-                    setText("paris");
-                  }}
-                >
-                  Paris
-                </button>
-                <button
-                  className="p-3 cursor-pointer"
-                  onClick={() => {
-                    setText("london");
-                  }}
-                >
-                  London
-                </button>
+                  Current location
+                </div>
+                <div className="w-full text-white grid grid-cols-4 ">
+                  <button
+                    className="border-r-1  p-3 cursor-pointer"
+                    onClick={() => {
+                      setText("seoul");
+                    }}
+                  >
+                    Seoul
+                  </button>
+                  <button
+                    className="p-3 cursor-pointer"
+                    onClick={() => {
+                      setText("tokyo");
+                    }}
+                  >
+                    Tokyo
+                  </button>
+                  <button
+                    className="border-x-1  p-3 cursor-pointer"
+                    onClick={() => {
+                      setText("paris");
+                    }}
+                  >
+                    Paris
+                  </button>
+                  <button
+                    className="p-3 cursor-pointer"
+                    onClick={() => {
+                      setText("london");
+                    }}
+                  >
+                    London
+                  </button>
+                </div>
               </div>
             </div>
           </div>
